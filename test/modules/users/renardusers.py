d6a6d5f90eba26d4a4f730adf7657b89732b4fac
jordanchiquet <jordanchiquet@gmail.com>
2020-02-27 18:20:58 -0600

    02-27-20 first multiplay commit

--- test/modules/users/renardusers.py
+++ test/modules/users/renardusers.py
@@ -1,19 +1,79 @@
 import csv
 import os
 import os.path
-
+import mysql.connector
 
 class renardusers:
-    def __init__(self, userid, param):
+    
+    def __init__(self, userid, field, param : str = None):
         self.userid = userid
+        self.field = field
         self.param = param
-        iddict = {"andrew": 201811169625899008,
-        "catherine": 583342254597472287,
-        "frank": 234381222334300162,
-        "joey": 172581464066490369,
-        "jordan": 191688156427321344}
     
-    # def renarduserwrite(self):
-    #     if not os.path.exists("/Users/jordanchiquet/personalandfinance/disbotren/test/modules/users/" + userid):
-            
+    def userinit(self):
+        mydb = mysql.connector.connect(
+        host='18.216.39.250',
+        user='dbuser',
+        passwd='e4miqtng')
+        mycursor = mydb.cursor()    
+        sql = "SELECT * FROM renarddb.users WHERE userid LIKE \"" + self.userid + "\""
+        mycursor.execute(sql)
+        for x in mycursor:
+            return(x)
     
+    def userexists(self):
+        if userinit() == "None":
+            return False
+        else:
+            return True
+
+    def userreg(self):
+        if userexists() == False:
+            print("user passed userexists redundancy test... registering user")
+            mydb = mysql.connector.connect(
+            host='18.216.39.250',
+            user='dbuser',
+            passwd='e4miqtng')
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO renarddb.users (user) VALUES (%s)"
+            val = [self.userid]
+            mycursor.execute(sql)
+            mydb.commit()
+            print("user " + self.userid + " registered.")
+            return("user " + self.userid + " registered.")
+        else:
+            print("user failed userexists redundancy test... either user is already registered or something is broken.")
+            return("user " + self.userid + " was not registered, either because the user is already registered or because something is broken.")
+
+# UPDATE `renarddb`.`users` SET `name` = 'sfsf' WHERE (`id` = '1');
+
+    def userread(self):
+        if userexists() == True:
+            mydb = mysql.connector.connect(
+            host='18.216.39.250',
+            user='dbuser',
+            passwd='e4miqtng')
+            mycursor = mydb.cursor()
+            sql = "SELECT (%s) FROM renarddb.users WHERE userid LIKE (%s)"
+            val = [self.field, self.userid]
+            mycursor.execute(sql)
+            for x in mycursor:
+                return(x)
+
+
+    def sqltest(self, user):
+        mydb = mysql.connector.connect(
+        host='18.216.39.250',
+        user='dbuser',
+        passwd='e4miqtng')
+        mycursor = mydb.cursor()
+        sql = "SELECT * FROM renarddb.quotes WHERE user LIKE \"" + user + "\""
+        mycursor.execute(sql)
+        for x in mycursor:
+            return(x)
+
+
+    
+testinit = renardusers(1,"fart")
+quicktest = testinit.sqltest("dog")
+print(quicktest)
