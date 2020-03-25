6241e02365b47e224b7790cd9f3cd132439f31e5
jordanchiquet <jordanchiquet@gmail.com>
2020-03-25 01:03:39 -0500

    for add to image search

--- modules/heycomputer.py
+++ modules/heycomputer.py
@@ -172,9 +172,9 @@ class heycomputer:
                 imgquerylist = removefirstindex(imgquerylist)
                 continue
         if genericsearch == False:
-            if imgquerylist[0] == "of" or imgquerylist[0] == "from":
+            if imgquerylist[0] == "of" or imgquerylist[0] == "from" or imgquerylist[0] == "for":
             #do something special for "from" later... search specific site or location
-                print("imgquerylist[0] was of or from, deleting imgquerylist[0]")
+                print("imgquerylist[0] was of or from or for, deleting imgquerylist[0]")
                 imgquerylist = removefirstindex(imgquerylist)
         if imgquerylist[0] == "a" or imgquerylist == "an":
             print("imgquerylist[0] was a or an, deleting imgquerylist[0]")
