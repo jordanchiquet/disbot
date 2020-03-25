50f251a3a07fdb4b7a2366a4b4605a30d660e3b5
jordan <jordanchiquet@gmail.com>
2020-03-25 00:58:44 -0500

    do search draft 1

--- modules/heycomputer.py
+++ modules/heycomputer.py
@@ -1,4 +1,5 @@
 from modules.googleimageapi import imageget
+from modules.googleapi import googleget
 from modules.definitionwebscrape import getdefinition
 from modules.listemptystring import listemptystring
 from modules.removefirstindex import removefirstindex
@@ -35,6 +36,9 @@ class heycomputer:
             print("command is for defintion")
             definitionresult = self.definitionexecute(intentkeyword)
             return("~" + definitionresult)
+        if intentkeyword == "do":
+            doresult = self.doexecute(intentkeyword)
+            return(doresult)
         else:
             if nocmdimagesearch == "True":
                 imageresult = self.imageexecute(intentindex, "nonspecific", True)
@@ -57,6 +61,18 @@ class heycomputer:
             msgspacesplit = removefirstindex(msgspacesplit)
         if msgspacesplit[0].startswith("comput") or msgspacesplit[0].startswith("compadre") or msgspacesplit[0].startswith("machine") or msgspacesplit[0].startswith("renard"):
             print("msgspacesplit[0] startwith variation of computer or renard name, deleting msgpacesplit [0]")
+            msgspacesplit = removefirstindex(msgspacesplit)
+        if msgspacesplit[0] == "go":
+            print("msgspacesplit[0] was go, deleting msgpacesplit [0]")
+            msgspacesplit = removefirstindex(msgspacesplit)
+        if msgspacesplit[0] == "ahead":
+            print("msgspacesplit[0] was ahead, deleting msgpacesplit [0]")
+            msgspacesplit = removefirstindex(msgspacesplit)
+        if msgspacesplit[0] == "and":
+            print("msgspacesplit[0] was and, deleting msgpacesplit [0]")
+            msgspacesplit = removefirstindex(msgspacesplit)
+        if msgspacesplit[0] == "download":
+            print("msgspacesplit[0] was download, deleting msgpacesplit [0]")
             msgspacesplit = removefirstindex(msgspacesplit)
         if msgspacesplit[0] == "load" or msgspacesplit[0] == "give":
             print("msgspacesplit[0] was load or give, deleting msgpacesplit [0]")
@@ -87,18 +103,26 @@ class heycomputer:
             return(nocmdimagesearch + "|" + " ".join(msgspacesplit))
 
 
-    def definitionexecute(self, defintioncommandsplitpoint):
-        definitionqueryorig = self.msgcontent.split(defintioncommandsplitpoint)[1]
+    def definitionexecute(self, definitioncommandsplitpoint):
+        definitionqueryorig = self.msgcontent.split(definitioncommandsplitpoint)[1]
         definitionquerylist = definitionqueryorig.split(" ")
         definitionquerylist = listemptystring(definitionquerylist)
-        if defintioncommandsplitpoint.startswith("what") or \
-        defintioncommandsplitpoint.startswith("waht") or \
-        defintioncommandsplitpoint == "wat" or \
-        defintioncommandsplitpoint == "wats":
+        if definitioncommandsplitpoint.startswith("what") or \
+        definitioncommandsplitpoint.startswith("waht") or \
+        definitioncommandsplitpoint == "wat" or \
+        definitioncommandsplitpoint == "wats":
             print("splitpoint was what... checking for is")
+            if definitionquerylist[0] == "if":
+                print("if parse")
             if definitionquerylist[0] == "is":
                 print("definitionquerylist[0] is is, deleting from defintionquerylist[0]")
                 definitionquerylist = removefirstindex(definitionquerylist)
+        # if definitioncommandsplitpoint == "look":
+        #     print("definitionquerylist[0] is look, deleting from defintionquerylist[0]")
+        #     definitionquerylist = removefirstindex(definitionquerylist)
+        #     if definitioncommandsplitpoint == "up":
+        #         print("definitionquerylist[0] is up, deleting from defintionquerylist[0]")
+        #         definitionquerylist = removefirstindex(definitionquerylist)
         if definitionquerylist[0] == "a" or definitionquerylist[0] == "an":
             print("definitionquerylist[0] is a or an, deleting from definitionquerlist[0]")
             definitionquerylist = removefirstindex(definitionquerylist)
@@ -107,12 +131,46 @@ class heycomputer:
         return(getdefinition(definitionquery))
 
 
-
+    def doexecute(self, dosplitpoint):
+        msglist = self.msgcontent.split(" ")
+        doqueryorig = self.msgcontent.split(dosplitpoint)[1]
+        doquerylist = doqueryorig.split(" ")
+        doquerylist = listemptystring(doquerylist)
+        if doquerylist[0] == "a" or doqueryorig[0] == "an":
+            print("doquerylist[0] was a or an, deleting first index]")
+            doquerylist = removefirstindex(doquerylist)
+        parseforimageresult = self.parseforimage(doquerylist[0])
+        if parseforimageresult.split("|")[0] == "True":
+            print("doquerylist[0] was image or filetype, setting imagesplit as doquerylist[0]")
+            if doquerylist[0] == "search":
+                print("doquerylist[0] was search, deleting first index]")
+                doquerylist = removefirstindex(doquerylist)
+            if doquerylist[0] == "for" or doquerylist[0] == "of":
+                print("doquerylist[0] was search, deleting first index]")
+                doquerylist = removefirstindex(doquerylist)
+            imagefiletype = parseforimageresult.split("|")[1]
+            imgquerystart = doquerylist[0]
+            imgqueryindex = msglist.index(imgquerystart)
+            imageresult = self.imageexecute(imgqueryindex, imagefiletype)
+            return(imageresult)
+        if doquerylist[0] == "search":
+            print('generic search execute')
+            doqueryindex = msglist.index(doquerylist[0])
+            dosearchresult = self.searchexecute(doqueryindex)
+            print("dosearchresult: [" + dosearchresult + "]")
+            return(dosearchresult)
+    
+        
     def imageexecute(self, imagecommandsplitpoint, filetype, genericsearch: bool = False):
         imgqueryorig = self.msgcontent.split(" ")
         imgquerylist = imgqueryorig[imagecommandsplitpoint:]
         print("starting imgquerylist: [" + str(imgquerylist) + "]")
         imgquerylist = listemptystring(imgquerylist)
+        for x in imagecmdlistfull:
+            if imgquerylist[0] == x:
+                print("imgquerylist[0] in imagecmdlist, removing")
+                imgquerylist = removefirstindex(imgquerylist)
+                continue
         if genericsearch == False:
             if imgquerylist[0] == "of" or imgquerylist[0] == "from":
             #do something special for "from" later... search specific site or location
@@ -127,6 +185,54 @@ class heycomputer:
             imgquery = " ".join(imgquerylist)
             print("starting imageget with query: [" + imgquery + "]")
             return(imageget(imgquery, filetype))
+    
+
+    def searchexecute(self, searchsplitpoint):
+        print("starting searchexecute")
+        searchqueryorig = self.msgcontent.split(" ")
+        searchquerylist = searchqueryorig[searchsplitpoint:]
+        print("starting searchquerylist: [" + str(searchquerylist) + "]")
+        searchquerylist = listemptystring(searchquerylist)
+        for x in searchcmdlistfull:
+            if searchquerylist[0] == x:
+                print("searchquerylist[0] in searchcmdlist, removing")
+                searchquerylist = removefirstindex(searchquerylist)
+                continue
+        print("escaped searchexecute searcmd loop")
+        if searchquerylist[0] == "up":
+            print("searchquerylist[0] was up, deleting searchquerylist[0]")
+            searchquerylist = removefirstindex(searchquerylist)
+        if searchquerylist[0] == "a" or searchquerylist[0] == "an":
+            print("searchquerylist[0] was a or an, deleting searchquerylist[0]")
+            searchquerylist = removefirstindex(searchquerylist)
+        if searchquerylist[0] == "for" or searchquerylist[0] == "of":
+            print("searchquerylist[0] was for or of, deleting searchquerylist[0]")
+            searchquerylist = removefirstindex(searchquerylist)
+        msglist = self.msgcontent.split(" ")
+        transitiontoimage = self.parseforimage(searchquerylist[0])
+        print("transition to image sanity: [" + transitiontoimage + "]")
+        print("made it out of image check after searchexecute")
+        searchqueryindex = msglist.index(searchquerylist[0])
+        print("searchqueryindex: [" + str(searchqueryindex) + "]")
+        print("that index is: [" + msglist[searchqueryindex] + "]")
+        if transitiontoimage.split("|")[0] == "True":
+            print("generic search is for an image! transitioning to image search")
+            imgfiletype = transitiontoimage[1]
+            imgfromsearchres = self.imageexecute(searchqueryindex, imgfiletype)
+            return(imgfromsearchres)
+        else:
+            print("not an image search after searchexecute :)")
+        if len(searchquerylist) < 1:
+            return("inv")
+        else:
+            print("made it to searchexecute else")
+            queryjoinindex = msglist[searchqueryindex:]
+            print("queryjoinindex : [" + str(queryjoinindex) + "]")
+            searchquery = " ".join(queryjoinindex)
+            print("searchquery: [" + searchquery + "]")
+        finalsearchresult = googleget(searchquery)
+        print("finalsearchresult: [" + finalsearchresult + "]")
+        return(finalsearchresult)
 
 
     def parsefordefinition(self, pfdkeyword):
@@ -137,7 +243,7 @@ class heycomputer:
         pfdkeyword.startswith("waht") or \
         pfdkeyword == "wat" or \
         pfdkeyword == "wats":
-            print("pfdkeyword was def or define or defintion or started with what, returning True")
+            print("pfdkeyword was def or define or defintion or started with what or was look, returning True")
             return True
         else:
             return False
@@ -145,6 +251,7 @@ class heycomputer:
 
     def parseforimage(self, pfikeyword):
         print("starting parseforimage")
+        print("pfikeyword: [" + pfikeyword + "]")
         if pfikeyword == "img" or pfikeyword == "image" or pfikeyword =="photo" or pfikeyword == "photograph" or pfikeyword == "pic" or pfikeyword == "picture" or pfikeyword == "snapshot":
             print("pfikeyword was img or image or photo or photograph or pic or picture or snapsho, setting isimage cmd to True, specificfilesearch to nonspecific")
             isimagecmd = "True"
@@ -161,3 +268,5 @@ class heycomputer:
         return(isimagecmd + "|" + specificfilesearch)
 
 
+imagecmdlistfull = ["img", "image", "photo", "photograph", "pic", "picture", "snaphot", "bmp", "gif", "jpg", "jpeg", "png"]
+searchcmdlistfull = ["bing", "google", "g", "ggl", "gogle", "duckduckgo", "search", "look"]
