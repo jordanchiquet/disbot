3ef77bdb3c84c2507494c4526caaec0d0e058136
jordan <jordanchiquet@gmail.com>
2020-03-28 16:28:04 -0500

    shadow dick hot fix

--- test/modules/dickshadow.py
+++ test/modules/dickshadow.py
@@ -9,7 +9,7 @@ def executeoverlay(imgpath):
         providedbackground = Image.open(imgpath)
         print("opened background")
         bgwidth, bgheight = providedbackground.size
-        print("background image size: [" + str(bgwidth) + ", " + str(bgheight) + "]")
+        print("background image sizE: [" + str(bgwidth) + ", " + str(bgheight) + "]")
         dsresize = (bgwidth, bgheight)
         dickshadow = dickshadow.resize(dsresize, Image.ANTIALIAS)
         providedbackground.paste(dickshadow, (0, 0), dickshadow)
@@ -17,16 +17,3 @@ def executeoverlay(imgpath):
         return("E:/disbot/picfolder/shadowdir/dickshadow.png")
     except:
         return("inv")
-
-
-
-#     @bot.command()
-# async def rev(ctx):
-#     await ctx.send("Working on it...")
-#     revquery = ctx.message.attachments[0].url
-#     response = google_images_download.googleimagesdownload()
-#     arguments = {"similar_images": revquery,"limit":1,"no_download":True}
-#     revresult = response.download(arguments)
-#     extractor = URLExtract()
-#     for url in extractor.gen_urls(str(revresult)):
-#         await ctx.send("Found this:\n" + url)
