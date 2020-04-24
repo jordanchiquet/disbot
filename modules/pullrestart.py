f9b103ff9944e9969d81b1dd6732f061bc4ed99c
jordan <jordanchiquet@gmail.com>
2020-04-24 14:03:29 -0500

    pull fix 2

--- modules/pullrestart.py
+++ modules/pullrestart.py
@@ -1,5 +1,5 @@
-import subprocess
+import os
 
 def pullrestart():
-    subprocess.call("git pull https://jordanchiquet:E4miqtng14+12@github.com/jordanchiquet/disbot.git")
-    subprocess.call("sudo systemctl restart disbotren")
+    os.system("git pull https://jordanchiquet:E4miqtng14+12@github.com/jordanchiquet/disbot.git")
+    os.system("sudo systemctl restart disbotren")
