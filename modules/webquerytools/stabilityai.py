7cfc50b2c538d801f864994c646f9b4689d90842
Jordan Chiquet <jordanchiquet@gmail.com>
2023-02-08 02:05:21 -0600

    error msg fix

--- modules/webquerytools/stabilityai.py
+++ modules/webquerytools/stabilityai.py
@@ -26,7 +26,7 @@ def generateStable(query):
                 print(
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
-                return("content error i have not gotten around yet sorry")
+                return("error: content/booby error i have not gotten around yet sorry")
             if artifact.type == generation.ARTIFACT_IMAGE:
                 img = Image.open(io.BytesIO(artifact.binary))
                 try:
